f3e0f42eeeb7fd8ffce0e5882c76687cd22fbecd
Khamthak <kkhamthakkonyak@gmail.com>
2026-08-27 14:50:20 +0530

    commited

--- scenes/pickup_item.gd
+++ scenes/pickup_item.gd
@@ -7,3 +7,7 @@ func _on_body_entered(body: Node3D) -> void:
 	if body is CharacterBody3D:
 		Inventory.add_item(item_name)
 		queue_free()
+
+
+func _on_body_exited(body: Node3D) -> void:
+	pass # Replace with function body.
--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -6,3 +6,11 @@ func add_item(item_name: String) -> void:
 	items.append(item_name)
 	print("Picked up: ", item_name)
 	print("Inventory now: ", items)
+	
+func remove_last_item() -> String:
+	if items.size() == 0:
+		return ""
+	var item_name = items.pop_back()
+	print("Took out: ", item_name)
+	print("Inventory now: ", items)
+	return item_name
